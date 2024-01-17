0848c81c4eb0246a57a503b2d4b6eea5661f5477
Tamilarasan M <tamizh1153@gmail.com>
2024-01-17 13:12:57 +0530

    Image Update

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "./intro.css"
-import intro_pic from '../../assets/Intro_pic.jpg'
+import intro_pic from "../../assets/IMG_6658.JPG"
 
 const Intro = () => {
   return (
